61fbe83876d1b69a2eaac059d67fbdf30771bfed
Rahul R <rahulram30122000@gmail.com>
2022-01-01 22:53:41 +0530

    Delete RouterApi directory

--- RouterApi/Areas/HelpPage/ModelDescriptions/DictionaryModelDescription.cs
+++
@@ -1,6 +0,0 @@
-namespace RouterApi.Areas.HelpPage.ModelDescriptions
-{
-    public class DictionaryModelDescription : KeyValuePairModelDescription
-    {
-    }
-}
--- RouterApi/Global.asax
+++
@@ -1,1 +0,0 @@
-﻿<%@ Application Codebehind="Global.asax.cs" Inherits="RouterApi.WebApiApplication" Language="C#" %>
